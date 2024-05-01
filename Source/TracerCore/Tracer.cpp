dac1ecfc64698020ff7339dbb96b29b943ad8ecf
NTB072\danvu <danvuk11@gmail.com>
2024-04-30 21:11:44 -0400

    Change triangle color

--- Source/TracerCore/Tracer.cpp
+++ Source/TracerCore/Tracer.cpp
@@ -96,7 +96,7 @@ namespace TraceCore
             renderPassInfo.renderArea.extent = _swapChain.getSwapChainExtent();
 
             std::array<VkClearValue, 2> clearValues{};
-            clearValues[0].color = {0.0f, 0.0f, 0.0f, 1.0f};
+            clearValues[0].color = {1.0f, 1.0f, 1.0f, 1.0f};
             clearValues[1].depthStencil = {1.0f, 0};
 
             renderPassInfo.clearValueCount = clearValues.size();
