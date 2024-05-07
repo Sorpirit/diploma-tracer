38980648db7b1af7969616223827871f610694c3
NTB072\danvu <danvuk11@gmail.com>
2024-05-06 20:41:32 -0400

    Add build vscode tasks

---
+++ Assets/Precompiler.py
@@ -0,0 +1,41 @@
+import os
+import subprocess
+
+compiler_path = "C:\VulkanSDK\1.3.280.0\Bin\glslc.exe"
+compiled_shader_postfix = ".spv"
+
+# Function to compile shader files
+def compile_shader(shader_file):
+    # Replace this command with your actual shell command for compiling shaders
+    compile_command = f"{compiler_path} {shader_file} -o {shader_file + compiled_shader_postfix}"
+    subprocess.run(compile_command, shell=True)
+
+# Path to the Shaders folder relative to this script
+shaders_folder = os.path.join(os.path.dirname(__file__), "Shaders")
+
+# Path to the PrecompiledShaders folder
+precompiled_folder = os.path.join(os.path.dirname(__file__), "PrecompiledShaders")
+
+# Check each shader file in the Shaders folder
+for shader_file in os.listdir(shaders_folder):
+    shader_path = os.path.join(shaders_folder, shader_file)
+    precompiled_shader_path = os.path.join(precompiled_folder, shader_file + compiled_shader_postfix)
+
+    # Check if the shader file exists in PrecompiledShaders folder
+    if os.path.exists(precompiled_shader_path):
+        # Compare modification times
+        shader_mtime = os.path.getmtime(shader_path)
+        precompiled_mtime = os.path.getmtime(precompiled_shader_path)
+
+        # If shader file is newer than precompiled shader, compile it
+        if shader_mtime > precompiled_mtime:
+            compile_shader(shader_path)
+            print(f"Compiled {shader_file}")
+        else:
+            print(f"Up-to-date {shader_file}")
+    else:
+        # If precompiled shader doesn't exist, compile shader
+        compile_shader(shader_path)
+        print(f"Compiled {shader_file}")
+
+print("Compilation complete")
--- Source/TracerCore/Tracer.cpp
+++ Source/TracerCore/Tracer.cpp
@@ -16,6 +16,7 @@ namespace TraceCore
 {
     struct SimplePushConstantData
     {
+        glm::mat2 transform{1.0f};
         glm::vec2 offset;
         alignas(16) glm::vec3 color;
     };
